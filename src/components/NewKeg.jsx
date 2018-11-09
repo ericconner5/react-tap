60f0bdbbd83ff90b03f954886fcc312436f477fb
Eric Conner <theluvq@gmail.com>
2018-11-09 13:22:52 -0800

    update boilerplate functions

--- src/components/NewKeg.jsx
+++ src/components/NewKeg.jsx
@@ -8,16 +8,33 @@ function NewKeg(){
       <form>
         <input
           type='text'
-          id='names'
-          placeholder='Pair Names'/>
+          id='newBrewery'
+          placeholder='Brewery Name'/>
         <input
           type='text'
-          id='location'
-          placeholder='Your Location'/>
-        <textarea
-          id='issue'
-          placeholder='Describish That Ish-ish.'/>
-        <button type='submit'>Help!</button>
+          id='newStyle'
+          placeholder='Beer style'/>
+        <input
+          type='text'
+          id='newAbv'
+          placeholder='ABV %'/>
+        <input
+          type='text'
+          id='newIbu'
+          placeholder='IBU'/>
+        <input
+          type='text'
+          id='newPrice'
+          placeholder='Price'/>
+        <input
+          type='text'
+          id='newPintCount'
+          placeholder='Pint Count'/>
+        <input
+          type='text'
+          id='newRegion'
+          placeholder='Region'/>
+        <button type='submit'>Add New Keg</button>
       </form>
     </div>
   );
