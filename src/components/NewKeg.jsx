f1740699c9b62201fb61b38fcaefc3b967dda24e
Eric Conner <theluvq@gmail.com>
2018-11-09 12:33:31 -0800

    setup router to newkeg page

--- src/components/NewKeg.jsx
+++ src/components/NewKeg.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
-// import { Keg } from './Keg';
+import PropTypes from 'prop-types';
+import Keg from './Keg';
 
 function NewKeg(){
   return(
